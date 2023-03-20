0e00d76815c50372fa24915b2eb32c32f95a2bba
Caid Buchman <caidbuch@ksu.edu>
2023-03-20 03:26:14 -0500

    playing with singleFileComponents in Vue

---
+++ src/components/BigYellowUsername.vue
@@ -0,0 +1,22 @@
+<template>
+    <p class="username">{{username}}</p>
+</template>
+
+<script>
+export default {
+    props: {
+        username: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
+
+<style lang="css" scoped>
+    .username {
+        color: #DCAE1D;
+
+    }
+</style>
+
